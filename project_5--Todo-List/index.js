dd375e3865ebe2c5e22ba8d4eca476ab33a62087
enoch-amart <amarteifioenoch4@gmail.com>
2024-10-14 19:11:03 +0100

    Rendering elements and deleting them

--- project_5--Todo-List/index.js
+++ project_5--Todo-List/index.js
@@ -1,22 +1,43 @@
-let items = [];
+let items = ["hello world", "damn"];
 
 const input = document.getElementById("itemInput");
 
-const itemElement = document.getElementById("item");
+const itemsDiv = document.getElementById("item");
 
-function clearItem() {
-  itemElement.innerHTML = "";
+function renderItems() {
+  itemsDiv.innerHTML = null;
+
+  for (const [idx, item] of Object.entries(items)) {
+    const container = document.createElement("div");
+    container.style.marginBottom = "1rem";
+
+    const text = document.createElement("p");
+    text.textContent = item;
+    text.style.display = "inline";
+    text.style.marginRight = "1rem";
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.onclick = () => removeItem(idx);
+
+    container.appendChild(text);
+    container.appendChild(button);
+
+    itemsDiv.appendChild(container);
+  }
 }
+
+renderItems();
+
+function clearItem() {}
 
 function loadItems() {}
 
-function renderItems() {}
-
 function saveItems() {}
 
-function addItem() {
-  itemElement.innerHTML = input.value;
-  input.value = "";
-}
+function addItem() {}
 
-function removeItem() {}
+function removeItem(idx) {
+  items.splice(idx, 1);
+  renderItems();
+}
