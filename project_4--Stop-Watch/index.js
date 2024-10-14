44895da6c9cee7ad97847573ed052e237d693a2f
enoch-amart <amarteifioenoch4@gmail.com>
2024-10-14 16:31:19 +0100

    finished the backend for project 4, project completed

--- project_4--Stop-Watch/index.js
+++ project_4--Stop-Watch/index.js
@@ -1,9 +1,36 @@
 let secondsElapsed = 0;
+
 let interval = null;
+
 const time = document.getElementById("time");
 
-function startClock() {}
+function padStart(value) {
+  return String(value).padStart(2, "0");
+}
 
-function stopClock() {}
+function setTime() {
+  const minutes = Math.floor(secondsElapsed / 60);
+  const seconds = secondsElapsed % 60;
 
-function resetClock() {}
+  time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
+}
+
+function timer() {
+  secondsElapsed++;
+  setTime();
+}
+
+function startClock() {
+  if (interval) stopClock();
+  interval = setInterval(timer, 1000);
+}
+
+function stopClock() {
+  clearInterval(interval);
+}
+
+function resetClock() {
+  stopClock();
+  secondsElapsed = 0;
+  setTime();
+}
